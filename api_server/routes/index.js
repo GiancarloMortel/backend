6963584c4d4a698261a6d19f2c0bc06215268aaa
geege <morteg1@unlv.nevada.edu>
2019-09-18 18:13:59 -0700

    updated route for stock

--- api_server/routes/index.js
+++ api_server/routes/index.js
@@ -3,13 +3,23 @@ const router = express.Router();
 const ctrlApi = require('../controllers/api');
 const cors = require('cors');
 
-// allows client to access api
-const corsOptions = {
+// allows client to access weather api
+const corsWeatherOptions = {
   origin: 'http://localhost:3000',
   optionsSuccessStatus: 200
 };
 
-router.get('/city/:city', cors(corsOptions), ctrlApi.cityList);
-router.get('/weather/:cityid', cors(corsOptions), ctrlApi.weatherDay);
+// used for weather app
+router.get('/city/:city', cors(corsWeatherOptions), ctrlApi.cityList);
+router.get('/weather/:cityid', cors(corsWeatherOptions), ctrlApi.weatherDay);
+
+// allows client to access stock api
+const corsStockOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+};
+
+// used for stock app
+router.get('/stock/:ticker', cors(corsStockOptions), ctrlApi.stockInfo);
 
 module.exports = router;
